889aba3c40b5fe500aca865c26f28122bc1e612a
BH267 <hassanbenydir22@gmail.com>
2025-11-16 10:46:29 +0100

    add epoll loop and accept/send/recv methods

--- server.cpp
+++ server.cpp
@@ -3,9 +3,18 @@
 #include <iostream>
 #include <netinet/in.h>
 #include <ostream>
-#include <sys/poll.h>
+#include <string>
+#include <sys/epoll.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include "server.hpp"
+
+void	Server::run(int port)
+{
+	if (init_first_sock(port))
+		return;
+	epoll_loop();
+}
 
 int set_nonblocking(int fd) {
     int flags = fcntl(fd, F_GETFL, 0);
@@ -16,7 +25,7 @@ int set_nonblocking(int fd) {
 
 int	Server::init_first_sock(int port)
 {
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd == -1)
 	{
 	    std::cerr << "socket() failed" << std::endl;
@@ -25,6 +34,7 @@ int	Server::init_first_sock(int port)
 	int opt = 1;
 	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)); // for "Address already in use" problem
 	set_nonblocking(sockfd);
+
 	struct sockaddr_in baddr;
 	memset(&baddr, 0, sizeof(baddr));
 	baddr.sin_family = AF_INET;
@@ -40,9 +50,122 @@ int	Server::init_first_sock(int port)
 		std::cerr << "[Error] listen fails" << std::endl;
 		return -1;
 	}
-	struct pollfd psockfd;
-	psockfd.fd = sockfd;
-	psockfd.events = POLL_IN;
-	_pollFds.push_back(psockfd);
+	// ghayt7ayed
+	_port = port;
 	return 0;
 }
+
+void	Server::accept_client()
+{
+	int client_fd = accept(sockfd, NULL, NULL);
+	if (client_fd != -1) {
+                set_nonblocking(client_fd);
+                _fds.push_back(client_fd);
+
+                struct epoll_event ev;
+                ev.events = EPOLLIN | EPOLLRDHUP;
+                ev.data.fd = client_fd;
+                epoll_ctl(epfd, EPOLL_CTL_ADD, client_fd, &ev);
+	}
+}
+
+void	Server::_recv(int fd)
+{
+	char		buff[4096]; // 4 kb hhhh
+	std::string	request;
+	ssize_t byts = recv(fd, buff, 1024, 0);
+
+	if (byts > 0)
+		request += buff;
+	else
+		throw byts;
+	// 	parce_http(request);
+}
+
+void	Server::_send(int fd)
+{
+	std::string	respons;// will come from parser
+	static size_t		offset;
+
+	ssize_t byts = send(fd, respons.c_str() + offset, respons.length() - offset, 0);
+	if (byts > 0)
+	{
+		offset += byts;
+		if (offset >= respons.length())
+			throw std::string("done");
+	}else
+		throw byts;
+
+}
+
+void	Server::epoll_loop()
+{
+	epfd = epoll_create(1);
+	if (-1 == epfd)
+		std::cerr << "[Error] epoll_create fails" << std::endl;return;
+
+	std::cout << "[DEBUG] sockfd: " << sockfd << std::endl;
+
+
+	struct	epoll_event ev;
+	ev.events = EPOLLIN;
+	ev.data.fd = sockfd;
+
+	epoll_ctl(epfd, EPOLL_CTL_ADD, sockfd, &ev);
+
+	while (true)
+	{
+		struct epoll_event clients[1000];
+		int n = epoll_wait(epfd, clients, 1000, -1);
+
+		for (int i=0; i < n; i++)
+		{
+			int fd = clients[i].data.fd;
+			if (fd == sockfd)
+				accept_client();
+			else
+			{
+				if (clients[i].events & (EPOLLERR | EPOLLHUP | EPOLLRDHUP))
+				{
+					epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL);
+					close(fd);
+					continue;
+				}
+				else if (clients[i].events & EPOLLIN)
+				{
+					try{
+						_recv(fd);
+					}catch (std::string e)
+					{
+						if (e == "ready")
+						{
+							ev.events = EPOLLOUT | EPOLLRDHUP;
+							epoll_ctl(epfd, EPOLL_CTL_MOD, fd, &ev);
+						}
+					}catch (ssize_t e)
+					{
+						epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL);
+						close(fd);
+						continue;
+					}
+				}
+				else if (clients[i].events & EPOLLOUT)
+				{
+					try{
+						_send(fd);
+					}catch (std::string e)
+					{
+						ev.events = EPOLLIN | EPOLLRDHUP;
+						epoll_ctl(epfd, EPOLL_CTL_MOD, fd, &ev);
+					}catch (ssize_t e)
+					{
+						epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL);
+						close(fd);
+						continue;
+					}
+				}
+			}
+		}
+	}
+}
+
--- server.hpp
+++ server.hpp
@@ -1,12 +1,23 @@
 #pragma once
 
 #include "include.hpp"
+#include <string>
 
 class	Server
 {
-	std::vector<struct pollfd> _pollFds;
+	int	_port;
+	std::vector<int> _fds;
+	int	sockfd;
+	int	epfd;
+
 	int	init_first_sock(int port);
+	void	epoll_loop();
+	void	accept_client();
+	void	_recv(int);
+	void	_send(int);
+	// ``` parser object ```
 	public:
+		
 		Server();
 		~Server();
 		void run(int port);
