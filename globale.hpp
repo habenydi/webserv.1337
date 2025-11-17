46c400f5470b8f6d93fe50dead8fb159fc8ec53e
Bouhcine Amezouar <bout2019ate@gmail.com>
2025-11-17 15:25:27 +0100

    parse config file

--- globale.hpp
+++ globale.hpp
@@ -4,40 +4,58 @@
 
 struct LocationConfig
 {
-    std::string path;
-    std::string root;
-    bool autoindex;
-    std::vector<std::string> allowed_methods;
-    std::string upload_store;
-    std::string cgi_extension;
+	std::string path;
+	std::string root;
+	bool autoindex;
+	std::vector<std::string> allowed_methods;
+	std::string upload_store;
+	std::string cgi_extension;
+	std::vector<std::string> index;
+	std::string cgi_pass;
+
+	// Constructor: initialize all members
+	LocationConfig()
+		: path(), root(), autoindex(false), allowed_methods(),
+		  upload_store(), cgi_extension(), index(), cgi_pass()
+	{
+	}
 };
 
 class globale
 {
-	public:
-		std::vector <std::string>	port;
-		std::string	server_name;
-		std::string path;
-		std::string	root;
-		unsigned long max_client_size;
-		bool autoindex;
-		std::vector<std::string> index;
-		std::map<int, std::string> error_page;
-		std::vector<LocationConfig> location;
-		globale& operator=(const globale& other)
-		{
-			if (this != &other)
-			{
-				this->path = other.path;
-				this->port = other.port;
-				this->root = other.root;
-				this->autoindex = other.autoindex;
-				this->index = other.index;
-				this->error_page = other.error_page;
-				this->location = other.location;
-				// if you add any thing int the private of class you have to add here
-			}
-			return (*this);
-		}
-};
+public:
+	std::vector<std::string> port;
+	std::string server_name;
+	std::string path;
+	std::string root;
+	unsigned long max_client_size;
+	bool autoindex;
+	std::vector<std::string> index;
+	std::map<int, std::string> error_page;
+	std::vector<LocationConfig> location;
 
+	// Constructor: initialize all members
+	globale()
+		: port(), server_name(), path(), root(),
+		  max_client_size(0), autoindex(false), index(),
+		  error_page(), location()
+	{
+	}
+
+	globale &operator=(const globale &other)
+	{
+		if (this != &other)
+		{
+			this->path = other.path;
+			this->port = other.port;
+			this->root = other.root;
+			this->autoindex = other.autoindex;
+			this->index = other.index;
+			this->error_page = other.error_page;
+			this->location = other.location;
+			this->server_name = other.server_name;
+			this->max_client_size = other.max_client_size;
+		}
+		return (*this);
+	}
+};
--- main.cpp
+++ main.cpp
@@ -10,129 +10,129 @@ int set_nonblocking(int fd) {
 }
 
 int main(int ac, char **av) {
-	// try
-	// {
-	// 	pars var;
-	// 	if (ac != 2)
-	// 		throw std::runtime_error("Usage: ./webserv <conf file>");
-	// 	globale data;
-	// 	var.parsing(av[1], data);
-	// 	return (1); // khliha bax ntesty
-	// 	const int PORT = 8080;
-	// 	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	// 	if (server_fd == -1) {
-	// 		std::cerr << "socket() failed\n";
-	// 		return 1;
-	// 	}
-
-	// 	// Allow reuse of address
-	// 	int opt = 1;
-	// 	setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-
-	// 	if (set_nonblocking(server_fd) == -1) {
-	// 		std::cerr << "fcntl failed\n";
-	// 		close(server_fd);
-	// 		return 1;
-	// 	}
-
-	// 	struct sockaddr_in addr;
-	// 	memset(&addr, 0, sizeof(addr));
-	// 	addr.sin_family = AF_INET;
-	// 	addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	// 	addr.sin_port = htons(PORT);
-
-	// 	if (bind(server_fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
-	// 		std::cerr << "bind() failed\n";
-	// 		close(server_fd);
-	// 		return 1;
-	// 	}
-
-	// 	if (listen(server_fd, 10) == -1) {
-	// 		std::cerr << "listen() failed\n";
-	// 		close(server_fd);
-	// 		return 1;
-	// 	}
-
-	// 	std::cout << "Server listening on port " << PORT << "\n";
-
-	// 	const char* response = 
-	// 		"HTTP/1.0 200 OK\r\n"
-	// 		"Content-Type: text/plain\r\n"
-	// 		"Content-Length: 18\r\n"
-	// 		"\r\n"
-	// 		"Hello, webserv!\n";
-
-	// 	struct pollfd fds[100]; // support up to 99 clients + 1 server
-	// 	int nfds = 1;
-	// 	fds[0].fd = server_fd;
-	// 	fds[0].events = POLLIN;
-
-	// 	while (true) {
-	// 		int ready = poll(fds, nfds, -1);
-	// 		if (ready <= 0) continue;
-
-	// 		// Check for new connection
-	// 		if (fds[0].revents & POLLIN) {
-	// 			struct sockaddr_in client_addr;
-	// 			socklen_t client_len = sizeof(client_addr);
-	// 			int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
-	// 			if (client_fd != -1) {
-	// 				if (set_nonblocking(client_fd) == -1) {
-	// 					close(client_fd);
-	// 				} else {
-	// 					// Add to poll list
-	// 					if (nfds < 100) {
-	// 						fds[nfds].fd = client_fd;
-	// 						fds[nfds].events = POLLOUT; // ready to send response immediately
-	// 						nfds++;
-	// 					} else {
-	// 						close(client_fd); // too many clients
-	// 					}
-	// 				}
-	// 			}
-	// 		}
-
-	// 		// Handle clients
-	// 		for (int i = 1; i < nfds; ) {
-	// 			if (fds[i].revents & (POLLERR | POLLHUP | POLLNVAL)) {
-	// 				close(fds[i].fd);
-	// 				fds[i] = fds[nfds - 1];
-	// 				nfds--;
-	// 				continue;
-	// 			}
-
-	// 			if (fds[i].revents & POLLOUT) {
-	// 				// Send hardcoded response
-	// 				send(fds[i].fd, response, strlen(response), 0);
-	// 				close(fds[i].fd);
-	// 				fds[i] = fds[nfds - 1];
-	// 				nfds--;
-	// 				continue;
-	// 			}
-	// 			i++;
-	// 		}
-	// 	}
-	// 	close(server_fd);
-	// }
-	// catch (std::exception& e)
-	// {
-	// 	std::cerr << e.what() << std::endl;
-	// 	return (1);
-	// }
 	try
 	{
-		(void)av;
-		(void)ac;
-		HttpRequest request;
-		int fd = open("httpReq", O_RDONLY);
-		httpPars http;
-		http.RequestPars(fd, request);
+		pars var;
+		if (ac != 2)
+			throw std::runtime_error("Usage: ./webserv <conf file>");
+		globale data;
+		var.parsing(av[1], data);
+		return (1); // khliha bax ntesty
+		const int PORT = 8080;
+		int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+		if (server_fd == -1) {
+			std::cerr << "socket() failed\n";
+			return 1;
+		}
+
+		// Allow reuse of address
+		int opt = 1;
+		setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+		if (set_nonblocking(server_fd) == -1) {
+			std::cerr << "fcntl failed\n";
+			close(server_fd);
+			return 1;
+		}
+
+		struct sockaddr_in addr;
+		memset(&addr, 0, sizeof(addr));
+		addr.sin_family = AF_INET;
+		addr.sin_addr.s_addr = htonl(INADDR_ANY);
+		addr.sin_port = htons(PORT);
+
+		if (bind(server_fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+			std::cerr << "bind() failed\n";
+			close(server_fd);
+			return 1;
+		}
+
+		if (listen(server_fd, 10) == -1) {
+			std::cerr << "listen() failed\n";
+			close(server_fd);
+			return 1;
+		}
+
+		std::cout << "Server listening on port " << PORT << "\n";
+
+		const char* response = 
+			"HTTP/1.0 200 OK\r\n"
+			"Content-Type: text/plain\r\n"
+			"Content-Length: 18\r\n"
+			"\r\n"
+			"Hello, webserv!\n";
+
+		struct pollfd fds[100]; // support up to 99 clients + 1 server
+		int nfds = 1;
+		fds[0].fd = server_fd;
+		fds[0].events = POLLIN;
+
+		while (true) {
+			int ready = poll(fds, nfds, -1);
+			if (ready <= 0) continue;
+
+			// Check for new connection
+			if (fds[0].revents & POLLIN) {
+				struct sockaddr_in client_addr;
+				socklen_t client_len = sizeof(client_addr);
+				int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
+				if (client_fd != -1) {
+					if (set_nonblocking(client_fd) == -1) {
+						close(client_fd);
+					} else {
+						// Add to poll list
+						if (nfds < 100) {
+							fds[nfds].fd = client_fd;
+							fds[nfds].events = POLLOUT; // ready to send response immediately
+							nfds++;
+						} else {
+							close(client_fd); // too many clients
+						}
+					}
+				}
+			}
+
+			// Handle clients
+			for (int i = 1; i < nfds; ) {
+				if (fds[i].revents & (POLLERR | POLLHUP | POLLNVAL)) {
+					close(fds[i].fd);
+					fds[i] = fds[nfds - 1];
+					nfds--;
+					continue;
+				}
+
+				if (fds[i].revents & POLLOUT) {
+					// Send hardcoded response
+					send(fds[i].fd, response, strlen(response), 0);
+					close(fds[i].fd);
+					fds[i] = fds[nfds - 1];
+					nfds--;
+					continue;
+				}
+				i++;
+			}
+		}
+		close(server_fd);
 	}
-	catch(const std::exception& e)
+	catch (std::exception& e)
 	{
-		std::cerr << e.what() << '\n';
-		return 1;
+		std::cerr << e.what() << std::endl;
+		return (1);
 	}
+	// try
+	// {
+	// 	(void)av;
+	// 	(void)ac;
+	// 	HttpRequest request;
+	// 	int fd = open("httpReq", O_RDONLY);
+	// 	httpPars http;
+	// 	http.RequestPars(fd, request);
+	// }
+	// catch(const std::exception& e)
+	// {
+	// 	std::cerr << e.what() << '\n';
+	// 	return 1;
+	// }
 	
 	return 0;
 }
